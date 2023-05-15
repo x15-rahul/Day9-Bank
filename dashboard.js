51bf7a7ab2545ed55bf27af161bde2b57c3429d9
x15-rahul <biswasrahul97@outlook.com>
2023-05-15 12:31:11 +0600

    withdraw functionality added

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,4 @@
+// deposit function
 document.getElementById('deposit-button').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
     const newDepositAmount = parseFloat(depositField.value);
@@ -16,4 +17,25 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
     depositField.value = '';
     
+})
+
+// withdraw function
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmount = parseFloat(withdrawField.value);
+    
+    const previousWithdrawAmountElement = document.getElementById('total-withdraw');
+    const previousWithdrawAmount = parseFloat(previousWithdrawAmountElement.innerText);
+
+    const newTotalWithdrawAmount = previousWithdrawAmount + newWithdrawAmount;
+    previousWithdrawAmountElement.innerText = newTotalWithdrawAmount;
+
+    const previousTotalBalanceElement = document.getElementById('total-balance');
+    const previousTotalBalance = parseFloat(previousTotalBalanceElement.innerText);
+
+    const newTotalBalance = previousTotalBalance - newWithdrawAmount;
+    previousTotalBalanceElement.innerText = newTotalBalance;
+
+
+    withdrawField.value = '';
 })
